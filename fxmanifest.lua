de1c37702544dfeb99577dbf633d4cd0bb5ae4b4
SUP2Ak <cormier.wesley@gmail.com>
2023-04-19 01:31:57 +0200

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+author 'SUP2Ak#3755'
+description 'supv_teleport-ipls (resource provisoir le temps que tout soit fait correctement de la repo property image)'
+
+version '0.1'
+
+client_scripts {
+    '@ox_lib/init.lua',
+    'client.lua'
+}
+
+depandencies {
+    'ox_lib',
+}
